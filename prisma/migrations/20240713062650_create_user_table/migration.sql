b9c6a99d570c7dd0ce1386fc7311d53d2dd5a30f
Gia Huy <huy.94.2016@gmail.com>
2024-07-13 13:38:44 +0700

    home : new environment at home

--- prisma/migrations/20240713062650_create_user_table/migration.sql
+++ prisma/migrations/20240713062650_create_user_table/migration.sql
@@ -10,7 +10,7 @@ CREATE TABLE "User" (
     "emailVerified" BOOLEAN NOT NULL DEFAULT false,
     "isAcitve" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
